133aa25fd91f9456b5ae2d68d5a9f35e87a7c879
gioKreator <giovanni_slayer@hotmail.com>
2023-03-03 01:37:17 -0600

    se sube la resta y el demo que pinta 2 estrellas

--- calculadora.py
+++ calculadora.py
@@ -4,4 +4,8 @@ num2 = 9
 def sum(number1, number2):
     return number1+number2
 
-print('La suma es: ',sum(num1, num2))
+def minus(number1, number2):
+    return number1-number2
+
+print('La suma es: ', sum(num1, num2))
+print('La resta es: ', minus(num1, num2))
